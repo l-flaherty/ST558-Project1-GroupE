dcf7dfdfb491a8a62f247a6f704284663de599b8
l-flaherty <lflaherty1@alum.babson.edu>
2024-06-05 21:45:59 -0400

    Initial setup of documents and folders.

---
+++ ST558 Project 1 R Script.R
@@ -0,0 +1,14 @@
+###########Written By Liam Flaherty And Jia Lu for ST558 Project 1##########
+#####1. Load In Data And Required Packages#####
+install.packages("tidyverse")
+install.packages("DBI")  
+install.packages("RSQLite")
+install.packages("Lahman")
+install.packages("readr")
+install.packages("readxl")
+library(tidyverse)
+library(DBI)
+library(RSQLite)
+library(Lahman)
+library(readr)
+library(readxl)
