aaf2811b51ca9cc96aa4eb6a9e70834d4e8358d6
l-flaherty <lflaherty1@alum.babson.edu>
2024-06-08 23:15:46 -0400

    Slight modifications to Jia's steps 5 and 6

--- ST558 Project 1 R Script.R
+++ ST558 Project 1 R Script.R
@@ -54,8 +54,6 @@ rm(yy)                                                       #no need to keep#
 
 
 
-
-
 ###1d. County vs. Non-county ###
 unique(census$area_name)                           #investigate data patterns#
 
@@ -68,21 +66,41 @@ unique(county$area_name)
 
 class(county)=c("county", class(county))           #not really sure why necessary#
 
+county=county |>
+  mutate(state=str_sub(area_name, nchar(county$area_name) - 1, nchar(county$area_name)))
 
+
+
+###1e. Get division###
+#From https://en.wikipedia.org/wiki/List_of_regions_of_the_United_States?useskin=vector#
+
+d1=c("CONNECTICUT", "MAINE", "MASSACHUSETTS", "NEW HAMPSHIRE", "RHODE ISLAND", "VERMONT")
+d2=c("NEW JERSEY", "NEW YORK", "PENNSYLVANIA")
+d3=c("ILLINOIS", "INDIANA", "MICHIGAN", "OHIO", "WISCONSIN")
+d4=c("IOWA", "KANSAS", "MINNESOTA", "MISSOURI", "NEBRASKA", "NORTH DAKOTA", "SOUTH DAKOTA")
+d5=c("DELAWARE", "FLORIDA", "GEORGIA", "MARYLAND", "NORTH CAROLINA", "SOUTH CAROLINA", "VIRGINIA", "DISTRICT OF COLUMBIA", "District of Columbia", "WEST VIRGINIA")
+d6=c("ALABAMA", "KENTUCKY", "MISSISSIPPI", "TENNESSEE")
+d7=c("ARKANSAS", "LOUISIANA", "OKLAHOMA", "TEXAS")
+d8=c("ARIZONA", "COLORADO", "IDAHO", "MONTANA", "NEVADA", "NEW MEXICO", "UTAH", "WYOMING")
+d9=c("ALASKA", "CALIFORNIA", "HAWAII", "OREGON", "WASHINGTON")
+
+districts=list(d1,d2,d3,d4,d5,d6,d7,d8,d9)
+division=vector()
+
+for (i in 1:nrow(noncounty)) {
+  j=1
+  
+  while(j<=length(districts) && !(noncounty$area_name[i] %in% districts[[j]])) {
+    j=j+1
+  }
+  
+  division[i]=ifelse(j<=length(districts), j, "ERROR")
+}
+
+noncounty$division=division
 
 
 
 
 
 #####2. Function Writing#####
-
-data_reshape=function(file) {
-  a=read_csv(file)
-  b=a |>
-    select(Area_name, STCOU, ends_with("D")) |>
-    rename(area_name=Area_name) |>
-    pivot_longer(cols=ends_with("D"),
-                 names_to="code",    
-                 values_to="observed")
-  return(b)
-}
